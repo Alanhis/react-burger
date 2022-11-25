f8cd37877edff3a5c62793c558e183ec84d63eae
Alanhis <ala1234765@gmail.com>
2022-11-25 19:50:03 +0500

    Исправление TS типа в Protected Route

--- src/components/component-routes/protected-route.tsx
+++ src/components/component-routes/protected-route.tsx
@@ -1,11 +1,14 @@
-import { Route, Redirect, useLocation } from 'react-router-dom';
+import { Route, Redirect, useLocation, RouteProps } from 'react-router-dom';
 
 import { ReactNode } from 'react';
 interface Props {
   children?: ReactNode;
   path: string;
 }
-export function ProtectedRoute({ children, ...rest }: Props) {
+export function ProtectedRoute({
+  children,
+  ...rest
+}: RouteProps & { children?: React.ReactNode }) {
   const location = useLocation().pathname;
   console.log('protected');
   return (
