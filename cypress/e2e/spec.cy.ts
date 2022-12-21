c66aad3b18de8ac69bf66f0c303fd00e84073935
Alanhis <ala1234765@gmail.com>
2022-12-21 23:12:25 +0500

    Исправление ошибки

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,26 +5,26 @@ describe('Проверка страницы <Конструктор>', () => {
   })
   it('Передача булочки', () => {
     const dataTransfer = new DataTransfer();
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
   })
   it('Смена булочек в конструкторе', () => {
     const dataTransfer = new DataTransfer();
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(0).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(0).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
   })
   it('Открытие модального окна', () => {
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).click()
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).click()
   })
   it('Закрытие модального окна', () => {
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).click()
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).click()
     cy.get('.closeButton').click()
   })
   it('Отображение данных в модальном окне', () => {
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).click()
-    cy.get('.modal_modal__sDNAf').contains("Флюоресцентная булка R2-D3")
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).click()
+    cy.get('[class^=modal_modal]').contains("Флюоресцентная булка R2-D3")
   })
   it('Проверка работоспособности модального окна заказа',()=>{
     cy.get('.profile').click()
@@ -32,13 +32,13 @@ describe('Проверка страницы <Конструктор>', () => {
     cy.get('input[type="password"]').type('123')
     cy.get('button[type="submit"]').click()
     const dataTransfer = new DataTransfer();
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(1).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(4).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
-    cy.get('.ingredient-card_foodCard__xXuPN ').eq(6).trigger('dragstart', {dataTransfer})
-    cy.get('.burger-constructor_IngredientList__nlhWm').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(1).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(4).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
+    cy.get('[class^=ingredient-card_foodCard]').eq(6).trigger('dragstart', {dataTransfer})
+    cy.get('[class^=burger-constructor_IngredientList]').trigger('drop',{dataTransfer})
     cy.get('.submit_order').click().wait(16000)
-    cy.get('.modal_modal__sDNAf').contains("Дождитель готовности на орбитальной станции")
+    cy.get('[class^=modal_modal]').contains("Дождитель готовности на орбитальной станции")
   })
 })
